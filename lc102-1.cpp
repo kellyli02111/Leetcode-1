b503e5562785de6b40902ec4360c776d6cd14d08
boyima <mbyhome@hotmail.com>
2015-12-07 21:04:10 -0800

    Create lc102-1.cpp

---
+++ lc102-1.cpp
@@ -0,0 +1,29 @@
+//两个queue的做法
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+        vector<int> res_temp;
+        if(root == NULL) return res;
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        q1.push(root);
+        while(!(q1.empty() && q2.empty())){
+            TreeNode* temp = q1.front();
+            q1.pop();
+            res_temp.push_back(temp->val);
+            if(temp->left != NULL){
+                q2.push(temp->left);
+            }
+            if(temp->right != NULL){
+                q2.push(temp->right);
+            }
+            if(q1.empty()){
+                res.push_back(res_temp);
+                res_temp.clear();
+                swap(q1, q2);
+            }
+        }
+        return res;
+    }
+};
