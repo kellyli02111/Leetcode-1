5f042250a5bd0ba19b55e534ee173f5ac27b7370
boyima <mbyhome@hotmail.com>
2016-02-08 22:43:19 -0800

    Update lc149.cpp

--- lc149.cpp
+++ lc149.cpp
@@ -14,14 +14,14 @@ public:
         int n = points.size();
         for(int i = 0; i < n; i++){
             unordered_map<double, int> slopeCount;
-            slopeCount[INT_MIN] = 0;
+            slopeCount[std::numeric_limits<double>::infinity()] = 0;
             int duplicate = 1;
             for(int j = i + 1; j < n; j++){
                 if((points[i].x == points[j].x) && (points[i].y == points[j].y)){
                     duplicate++;
                 }
                 else if(points[i].x == points[j].x){
-                    slopeCount[INT_MAX]++;
+                    slopeCount[std::numeric_limits<double>::infinity()]++;
                 }
                 else{
                     double slope = (double)(points[j].y - points[i].y) / (points[j].x - points[i].x);
