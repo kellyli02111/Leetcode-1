d472d8ae699577d2b5aa6946bebf1732e2aa8990
boyima <mbyhome@hotmail.com>
2016-05-16 01:02:21 -0700

    Create lc039.cpp

---
+++ lc039.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        int sum = 0;
+        vector<int> solution;
+        vector<vector<int>> result;
+        combination(candidates, target, sum, 0, solution, result);
+        return result;
+    }
+    
+    void combination(vector<int>& candidates, int target, int& sum, int level, vector<int>& solution, vector<vector<int>>& result){
+        if(sum > target) return;
+        if(sum == target){
+            result.push_back(solution);
+            return;
+        }
+        for(int i = level; i < candidates.size(); i++){
+            solution.push_back(candidates[i]);
+            sum = sum + candidates[i];
+            combination(candidates, target, sum, i, solution, result);
+            sum = sum - candidates[i];
+            solution.pop_back();
+        }
+    }
+};
