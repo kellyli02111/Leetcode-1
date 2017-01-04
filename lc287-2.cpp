43477b5111ccf8ed51cd3aa0bb570399181c43c0
boyima <mbyhome@hotmail.com>
2017-01-03 23:34:46 -0800

    Create lc287-2.cpp

---
+++ lc287-2.cpp
@@ -0,0 +1,21 @@
+//http://bookshadow.com/weblog/2015/09/28/leetcode-find-duplicate-number/
+//Time: O(n);
+//Space: O(1);
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = 0;
+        int fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow == fast) break;
+        }
+        slow = 0;
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
