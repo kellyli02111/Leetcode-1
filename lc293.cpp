851bc8ca6710948b9cf15ca912d1c93eb01b9960
boyima <mbyhome@hotmail.com>
2017-01-04 23:14:15 -0800

    Create lc293.cpp

---
+++ lc293.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<string> generatePossibleNextMoves(string s) {
+        vector<string> res;
+        if(s.size() < 2) return res;
+        for(int i = 0; i < s.size() - 1; i++){
+            if(s[i] == '+' && s[i + 1] == '+'){
+                s[i] = '-';
+                s[i + 1] = '-';
+                res.push_back(s);
+                s[i] = '+';
+                s[i + 1] = '+';
+            }
+        }
+        return res;
+    }
+};
