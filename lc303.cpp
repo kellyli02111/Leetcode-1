986b9a96bf3e53a20c0f7c82208e0c746f8d2366
boyima <mbyhome@hotmail.com>
2017-02-02 01:04:06 -0800

    Update lc303.cpp

--- lc303.cpp
+++ lc303.cpp
@@ -1,7 +1,7 @@
 class NumArray {
-    vector<int> numSum;
 public:
-    NumArray(vector<int> &nums) {
+    vector<int> numSum;
+    NumArray(vector<int> nums) {
         if(!nums.empty()){
             numSum.push_back(nums[0]);
             for(int i = 1; i < nums.size(); i++){
@@ -9,9 +9,15 @@ public:
             }
         }
     }
-
+    
     int sumRange(int i, int j) {
         if(i == 0) return numSum[j];
         return (numSum[j] - numSum[i - 1]);
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
