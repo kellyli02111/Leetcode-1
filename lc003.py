f2ca6277fa1beece5dae927d65db8122c56139be
boyima <mbyhome@hotmail.com>
2017-08-19 00:15:00 -0700

    Create lc003.py

---
+++ lc003.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        start = 0
+        end = 0
+        myDict = {}
+        res = 0
+        n = len(s)
+        if(s == ""):
+            return 0
+        while(end < n):
+            if(myDict.get(s[end]) != None):
+                del myDict[s[start]]
+                start = start + 1
+            else:
+                myDict[s[end]] = 1
+                end = end + 1
+            res = max(res, (end - start))
+        return res
+            
